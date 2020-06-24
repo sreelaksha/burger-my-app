2c76929d9b27d2f794f8d2d1fda621881a741b8d
Sreelaksha <sreelaksha.s@gmail.com>
2020-06-24 22:49:41 +0200

    created Layout component ,added hoc- aux.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import Layout from './components/Layout/Layout.js';
 function App() {
   return (
     <div>
-
+        <Layout>
+            <p> testing </p>
+        </Layout>
     </div>
   );
 }
