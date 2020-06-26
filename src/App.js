9dc47a129749396e081b9c7cedc659d82ca5bc0f
Sreelaksha <sreelaksha.s@gmail.com>
2020-06-26 17:16:56 +0200

    implemented burger builder container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Layout from './components/Layout/Layout.js';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
 function App() {
   return (
     <div>
-        <Layout>
-            <p> testing </p>
-        </Layout>
+        <Layout> </Layout>
+        <BurgerBuilder/>
     </div>
   );
 }
