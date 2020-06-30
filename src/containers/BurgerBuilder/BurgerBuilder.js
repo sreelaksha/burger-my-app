6798c3e616551f1cac0da5da6cfab52419d3d602
Sreelaksha <sreelaksha.s@gmail.com>
2020-06-30 16:19:37 +0200

    created Order Summary Modal , Modal and Modal css

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
     const INGREDIENT_PRICES = {
         bacon : 0.4,
@@ -79,6 +81,9 @@ class BurgerBuilder extends Component {
         //{salad: true, bacon : false,...}
         return(
             <Aux>
+                <Modal>
+                    <OrderSummary ingredients = {this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
                 <BuildControls
                     addedIngredients = {this.addIngredientHandler}
