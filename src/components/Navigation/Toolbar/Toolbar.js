30e4a40d7c79d10630276faacdf838b2a2794598
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-01 16:48:46 +0200

    creating a responsive SideDrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -5,7 +5,7 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = (props) => (
         <header className = {styles.Toolbar}>
-            <div> MENU </div>
+            <div style = {{color: 'white'}}> MENU </div>
             <Logo/>
             <nav>
                 <NavigationItems />
