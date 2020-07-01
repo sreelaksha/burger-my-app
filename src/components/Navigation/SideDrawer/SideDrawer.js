a0021dc8a378c1249cf02f884a5da6e63206402f
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-01 23:33:59 +0200

    dismissing the sideDrawer on clicking on the Backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,13 +6,16 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
-        //...
+        let attachedStyles = [styles.SideDrawer, styles.Close];
+        if(props.open){
+            attachedStyles = [styles.SideDrawer, styles.Open];
+        }
         return(
                 <Aux>
                 <Backdrop
                         show = {props.open}
                          clicked = { props.closed}/>
-                    <div className = {styles.SideDrawer}>
+                    <div className = {attachedStyles.join(' ')}>
                         <div className = {styles.Logo}>
                                 <Logo />
                         </div>
