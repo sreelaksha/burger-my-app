4f2e9e7fd421ebc7b6c71a5ea9bc86326bff5727
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-06 16:17:20 +0200

    Handling Errors with wrong url

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
     const INGREDIENT_PRICES = {
         bacon : 0.4,
@@ -98,7 +99,7 @@ class BurgerBuilder extends Component {
                             deliveryMethod : 'fastest',
                      }
            }
-            axios.post('/orders.json ' , order)
+            axios.post('/orders' , order)
                 .then (response =>{
                       this.setState({ loading : false, purchasing : false })
                 })
@@ -146,4 +147,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
