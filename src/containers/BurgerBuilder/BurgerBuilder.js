625e6114f91c42b6797c264e2e6437c8b362ce5e
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-06 11:33:03 +0200

    sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
     const INGREDIENT_PRICES = {
         bacon : 0.4,
@@ -80,7 +81,27 @@ class BurgerBuilder extends Component {
      }
 
      orderContinuedHandler = () => {
-            alert('You continued...');
+           // alert('You continued...');
+           const order = {
+                    ingredients : this.state.ingredients,
+                    price : this.state.totalPrice,
+                    customer : {
+                            name: 'laksha',
+                            email : 'test@test.com',
+                            address: {
+                                 street: 'No.57',
+                                 zipCode: 94874
+                            },
+                            deliveryMethod : 'fastest'
+                     }
+           }
+            axios.post('/orders.json ' , order)
+                .then (response =>{
+                    console.log(response);
+                })
+                .catch (error => {
+                       console.log(error);
+                })
      }
 
     render() {
