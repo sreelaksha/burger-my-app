cf2fcaecc7ff17d765f2ae319fa79093d6e3f6cd
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-14 12:22:46 +0200

    fetching ingredients name and value and passing it to Ingredients

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,39 @@ import React from 'react';
 
 import styles from './Order.module.css';
 
-const order = (props) => (
-        <div className = {styles.Order}>
-            <p> Ingredients:Salad (1) </p>
-            <p> Price: <strong> INR 500 </strong></p>
-        </div>
-);
+const order = (props) => {
+        const ingredients =[];
+        for(let ingredientName in props.ingredients){
+                //console.log(props.ingredients);
+                ingredients.push(
+                    {
+                        name: ingredientName,
+                        value: props.ingredients[ingredientName]
+                    }
+                );
+                //console.log(props.ingredients[ingredientName]);
+         }
+        const ingredientsOutput = ingredients.map(ig => {
+            return <span
+                    style = {{
+                        textTransform: 'capitalize',
+                        display: 'inline-block',
+                        margin:'0 8px',
+                       border: '1px solid #ccc',
+                       padding: '5px'
+                    }}
+                key = {ig.name} >
+                {ig.name} ({ig.value})
+                 </span>;
+        });
+
+        return(
+            <div className = {styles.Order}>
+                <p> Ingredients: {ingredientsOutput}</p>
+                <p> Price: <strong> {props.price.toFixed(2)}</strong></p>
+            </div>
+        );
+ }
+
 
 export default order;
