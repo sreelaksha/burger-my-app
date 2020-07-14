6566c0363e7f7533a344babe733e36cc1df01580
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-14 23:36:39 +0200

    fixing the common validation Gotcha - fixed minLength maxLength error of zipCode

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -76,16 +76,16 @@ import Input from '../../../components/UI/Input/Input';
         }
 
     checkValidity(value, rules){
-        let isValid = false;
+        let isValid = true;
 
         if(rules.required){
-            isValid = value.trim() !== '';
+            isValid = value.trim() !== '' && isValid;
         }
         if(rules.minLength){
-            isValid = value.length >= rules.minLength;
+            isValid = value.length >= rules.minLength && isValid;
         }
         if(rules.maxLength){
-            isValid = value.length <= rules.maxLength;
+            isValid = value.length <= rules.maxLength && isValid;
         }
         return isValid;
     }
