3e85eb66d0430d0c17d0b41fa12bfa0663f539df
Sreelaksha <sreelaksha.s@gmail.com>
2020-07-14 10:45:45 +0200

    fetching Orders - converting Orders Object to Order Arrays

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,10 +1,41 @@
 import React , { Component } from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
     class Orders extends Component{
+        state = {
+            orders : [],
+            loading : true
+        }
+
+        componentDidMount(){
+            axios.get('/orders.json')
+                .then(response => {
+                    const fetchedOrders= [];
+                    for(let key in response.data){
+                                console.log('Key:'+ key);
+                                console.log('Response-data :'+ response.data)
+                       fetchedOrders.push({
+                           ...response.data[key],
+                           id: key
+                       });
+                       console.log('response-data-key' +response.data[key]);
+                    }
+                       console.log('fetchedOrders:'+ fetchedOrders);
+                       console.log(response.data);
+
+                       this.setState({loading: false, orders: fetchedOrders});
+                })
+                .catch(error => {
+                    this.setState({loading: false});
+                })
+        }
 
         render(){
+
+
             return(
                 <div>
                     <Order />
@@ -14,4 +45,4 @@ import Order from '../../components/Order/Order';
         }
     }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
