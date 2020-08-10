8639a9a65bb3b0db290e8e2b46af7fda3aecee62
Sreelaksha <sreelaksha.s@gmail.com>
2020-08-10 17:30:48 +0200

    Updates

---
+++ precache-manifest.01bb933d49475f1941fe89b9cfc8bf2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14364023977262831fa7d2723152c6a3",
+    "url": "/my-burger-app/index.html"
+  },
+  {
+    "revision": "a8b1f0ce1053990ffc18",
+    "url": "/my-burger-app/static/css/main.2fb58618.chunk.css"
+  },
+  {
+    "revision": "9f701c93ada3f5613a00",
+    "url": "/my-burger-app/static/js/2.4c524ab2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-burger-app/static/js/2.4c524ab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b1f0ce1053990ffc18",
+    "url": "/my-burger-app/static/js/main.c7faeaf0.chunk.js"
+  },
+  {
+    "revision": "877c21608333be838083",
+    "url": "/my-burger-app/static/js/runtime-main.fa9aa313.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/my-burger-app/static/media/burger-logo.b8503d26.png"
+  }
+]);
